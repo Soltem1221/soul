defea622030069fcaa49b1b824a08a3811d168b3
Soltem1221 <solimontemesgen@gmail.com>
2025-10-30 11:28:51 -0700

    Add files via upload

---
+++ calcu.js
@@ -0,0 +1,6 @@
+// const items=['abebe','sol','beza','zele'];
+//   for(const item of items){
+//       console.log(item);
+//   }
+  const num=[1,2,3,4,5];
+  const cal=num.filter((number)=>number%2===0);console.log(cal);
